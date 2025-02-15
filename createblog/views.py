d1b4969d4b06e6bf98260cf1a78950a15363077b
manubg <bgmanu2426@gmail.com>
2025-02-15 14:33:41 +0530

    Add views for listing and creating blog posts

--- createblog/views.py
+++ createblog/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect
+from .models import Blog
+from .forms import BlogForm
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def ViweBlog(request):
+    blogs = Blog.objects.all().order_by('-date_posted')
+    return render(request, 'blogs.html', {'blogs': blogs})
+
+def CreateBlog(request):
+    if request.method == 'POST':
+        form = BlogForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save(commit=False)
+            form.author = request.user
+            form.save()
+            return redirect('ViweBlog')
+    else:
+        form = BlogForm()
+    return render(request, 'createblog.html', {'form': form})
