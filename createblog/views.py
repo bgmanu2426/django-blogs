eef8c9189fa532465a78f05cc16cf4b51447b444
manubg <bgmanu2426@gmail.com>
2025-02-15 17:04:47 +0530

    Add edit and delete functionality for blog posts

--- createblog/views.py
+++ createblog/views.py
@@ -4,12 +4,16 @@ from .models import Blog
 from .forms import BlogForm
 
 # Create your views here.
+
+
 def index(request):
     return render(request, 'index.html')
+
 
 def ViweBlog(request):
     blogs = Blog.objects.all().order_by('-date_posted')
     return render(request, 'blogs.html', {'blogs': blogs})
+
 
 def CreateBlog(request):
     if request.method == 'POST':
@@ -22,3 +26,25 @@ def CreateBlog(request):
     else:
         form = BlogForm()
     return render(request, 'createblog.html', {'form': form})
+
+
+def editBlog(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id, user=request.user)
+    if request.method == 'POST':
+        form = BlogForm(request.POST, request.FILES, instance=blog)
+        if form.is_valid():
+            form.save(commit=False)
+            form.author = request.user
+            form.save()
+            return redirect('ViweBlog')
+    else:
+        form = BlogForm(instance=blog)
+    return render(request, 'editblog.html', {'form': form})
+
+
+def deleteBlog(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id, user=request.user)
+    if request.method == 'POST':
+        blog.delete()
+        return redirect('ViweBlog')
+    return render(request, 'deleteblog.html', {'blog': blog})
